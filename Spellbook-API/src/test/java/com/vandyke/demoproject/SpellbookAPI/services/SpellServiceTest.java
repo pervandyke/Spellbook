8796dd454f202145f176c92d63bebf0e2c080e32
Per Van Dyke <sulu244@gmail.com>
2022-08-09 14:00:03 -0400

    Test Class with Placeholders

---
+++ Spellbook-API/src/test/java/com/vandyke/demoproject/SpellbookAPI/services/SpellServiceTest.java
@@ -0,0 +1,56 @@
+package com.vandyke.demoproject.SpellbookAPI.services;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class SpellServiceTest {
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+
+  @Test
+  public void createSpellTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void deleteSpellTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void editSpellTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void getSpellByIdTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void getSpellsTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void getUserSpellsTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+  
+  @Test
+  public void dataToSpellTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void spellToDataTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
