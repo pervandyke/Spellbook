9f6e94131bfbd56422c823e681fef3ece9d5e17b
Per Van Dyke <sulu244@gmail.com>
2022-08-10 17:15:42 -0400

    Initial Test
    Finally figured out how to set up tests properly.

--- Spellbook-API/src/test/java/com/vandyke/demoproject/SpellbookAPI/services/SpellServiceTest.java
+++ Spellbook-API/src/test/java/com/vandyke/demoproject/SpellbookAPI/services/SpellServiceTest.java
@@ -2,6 +2,7 @@ package com.vandyke.demoproject.SpellbookAPI.services;
 
 import org.testng.annotations.Test;
 
+import com.vandyke.demoproject.SpellbookAPI.exeptions.UserNotFoundException;
 import com.vandyke.demoproject.SpellbookAPI.frontEndObjects.SpellData;
 import com.vandyke.demoproject.SpellbookAPI.models.Spell;
 import com.vandyke.demoproject.SpellbookAPI.models.User;
@@ -10,13 +11,18 @@ import com.vandyke.demoproject.SpellbookAPI.repositories.UserRepository;
 
 import org.testng.annotations.BeforeClass;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 @Test
@@ -43,12 +49,18 @@ public class SpellServiceTest extends AbstractTestNGSpringContextTests {
 
 
     @Test
-    public void givenValidData_whenAddSpellButtonClicked_thenSpellShouldBeSaved() {
+    public void givenValidData_whenAddSpellButtonClicked_thenSpellShouldBeSaved() throws UserNotFoundException {
         SpellData data = createDummySpellData();
         Spell spell = createDummySpell();
+        Optional<User> user = createDummyUser();
 
         when(spellRepo.save(any())).thenReturn(spell);
+        when(userRepo.findById(any())).thenReturn(user);
 
+        Spell returnedSpell = spellService.createSpell(data);
+        assertEquals(returnedSpell, spell);
+
+        verify(spellRepo).save(spell);
     }
 
     @Test
@@ -90,6 +102,7 @@ public class SpellServiceTest extends AbstractTestNGSpringContextTests {
         SpellData data = new SpellData();
 
         data.setName("TestSpell");
+        data.setId(1l);
         data.setUserId(1l);
         data.setLevel(2);
         data.setRange("100ft");
@@ -108,7 +121,7 @@ public class SpellServiceTest extends AbstractTestNGSpringContextTests {
 
         data.setName("TestSpell");
         data.setId(1l);
-        data.setUser(createDummyUser());
+        data.setUser(createDummyUser().get());
         data.setLevel(2);
         data.setRange("100ft");
         data.setCastingTime("1 Action");
@@ -121,13 +134,15 @@ public class SpellServiceTest extends AbstractTestNGSpringContextTests {
         return data;
     }
 
-    private User createDummyUser() {
+    private Optional<User> createDummyUser() {
         User user = new User();
 
         user.setId(1l);
         user.setUsername("test");
         user.setPassword("pass");
 
-        return user;
+        Optional<User> op = Optional.of(user);
+
+        return op;
     }
 }
